cea2cf8628d5d7293dfe3c61316c90c9df51a7d8
Tommy-Burns <58440453+Tommy-Burns@users.noreply.github.com>
2022-10-09 19:00:15 +0000

    user profile with edits complete

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
     
     # path('login/', views.user_login, name='login'),
     
