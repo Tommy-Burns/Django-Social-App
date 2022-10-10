2ee43612ee7f161c77430f8853f0566ad13084d3
Tommy-Burns <58440453+Tommy-Burns@users.noreply.github.com>
2022-10-10 22:06:21 +0000

    follow and unfollow system created

--- PicBook/settings.py
+++ PicBook/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+
+from django.urls import reverse_lazy
 from .env.details import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -170,3 +172,8 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 ]
+
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
